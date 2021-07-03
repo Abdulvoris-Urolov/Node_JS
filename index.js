4072a7c936ffc61e980b05b9f64c4752e61b7d17
DarkWeb <80630475+Abdulvorisjs@users.noreply.github.com>
2021-07-03 10:32:03 +0500

    add

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports.add = function add(a, b){
+    return a+b;
+}
+
+module.exports.substrack = function substrack(a, b){
+    return a-b;
+}
+
+module.exports.multiply = function multiply(a, b){
+    return a*b;
+}
